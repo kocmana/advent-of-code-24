5c12667a1cb5f904cf4a6f0f1c54d90028c4881a
Andreas Kocman <andreas.kocman@gmail.com>
2025-02-11 22:01:27 +0100

    10.12 - Hoof It Refactoring

--- src/main/kotlin/at/kocmana/aoc24/dec10/HoofIt1.kt
+++ src/main/kotlin/at/kocmana/aoc24/dec10/HoofIt1.kt
@@ -6,24 +6,22 @@ const val maxValue = 9
 
 fun main() {
     val board = Board("/dec10/HoofIt.txt") { it.toInt() }
-    val startingPoints = findStartingPoints(board)
-    val result = calculatePaths(board, startingPoints)
+    val startingPoints = board.findStartingPoints(board)
+    val result = board.calculatePaths(startingPoints)
     println(result)
 }
 
-fun findStartingPoints(board: Board<Int>) =
-    board.findAllPositionsWhere { it == 0 }
+fun Board<Int>.findStartingPoints(board: Board<Int>) =
+    this.findAllPositionsWhere { it == 0 }
 
-fun calculatePaths(board: Board<Int>, startingPoints: List<Board<Int>.Position>) =
+fun Board<Int>.calculatePaths(startingPoints: List<Board<Int>.Position>) =
     startingPoints.asSequence()
-        .map { iterateToNextLevel(board, 1, it) }
+        .map { this.iterateToNextLevel(1, it) }
         .map { it.size }
         .sum()
 
-fun iterateToNextLevel(board: Board<Int>, value: Int, position: Board<Int>.Position): Set<Board<Int>.Position> {
-    if (position.getValue() >= maxValue) {
-        return setOf(position)
-    }
+fun Board<Int>.iterateToNextLevel(value: Int, position: Board<Int>.Position): Set<Board<Int>.Position> {
+    if (position.getValue() >= maxValue) return setOf(position)
 
     val nextPositions = position.getApplicableDirections { it == value }.asSequence()
         .map { position andOneStepTo it }
@@ -32,6 +30,6 @@ fun iterateToNextLevel(board: Board<Int>, value: Int, position: Board<Int>.Posit
     if (nextPositions.isEmpty()) return emptySet()
 
     return nextPositions.asSequence()
-        .flatMap { iterateToNextLevel(board, value + 1, it) }
+        .flatMap { this.iterateToNextLevel(value + 1, it) }
         .toSet()
 }
--- src/main/kotlin/at/kocmana/aoc24/dec10/HoofIt2.kt
+++ src/main/kotlin/at/kocmana/aoc24/dec10/HoofIt2.kt
@@ -4,20 +4,18 @@ import at.kocmana.helper.Board
 
 fun main() {
     val board = Board("/dec10/HoofIt.txt") { it.toInt() }
-    val startingPoints = findStartingPoints(board)
-    val result = calculatePathsPart2(board, startingPoints)
+    val startingPoints = board.findStartingPoints(board)
+    val result = board.calculatePathsPart2(startingPoints)
     println(result)
 }
 
-fun calculatePathsPart2(board: Board<Int>, startingPoints: List<Board<Int>.Position>) =
+fun Board<Int>.calculatePathsPart2(startingPoints: List<Board<Int>.Position>) =
     startingPoints.asSequence()
-        .map { iterateToNextLevelPart2(board, 1, it) }
+        .map { this.iterateToNextLevelPart2(1, it) }
         .sum()
 
-fun iterateToNextLevelPart2(board: Board<Int>, value: Int, position: Board<Int>.Position): Int {
-    if (position.getValue() >= maxValue) {
-        return 1
-    }
+fun Board<Int>.iterateToNextLevelPart2(value: Int, position: Board<Int>.Position): Int {
+    if (position.getValue() >= maxValue) return 1
 
     val nextPositions = position.getApplicableDirections { it == value }.asSequence()
         .map { position andOneStepTo it }
@@ -26,6 +24,6 @@ fun iterateToNextLevelPart2(board: Board<Int>, value: Int, position: Board<Int>.
     if (nextPositions.isEmpty()) return 0
 
     return nextPositions.asSequence()
-        .map { iterateToNextLevelPart2(board, value + 1, it) }
+        .map { this.iterateToNextLevelPart2(value + 1, it) }
         .sum()
 }
